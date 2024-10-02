9ff035339e9de85320e704962ab2dbc0cf71db0e
Dario Sanchez <dariosanchez351@gmail.com>
2024-10-02 12:42:54 +0200

    jijiji

--- Unidad2/Ejercicios/ejercicio3/ejercicio3.js
+++ Unidad2/Ejercicios/ejercicio3/ejercicio3.js
@@ -1,3 +1,16 @@
+/*
+*Haz un programa que sirva para dividir dos números. ¿Qué sucede si en lugar de dividir dos 
+*números, intentamos dividir dos textos? ¿Qué sucede si el divisor es el número 0? ¿Y si no introducimos nada?
+*/
+
+function ejercicio(){
 let numero1=2;
 let numero2=3;
+let texto1="dasfasdf";
+let texto2="kljfhdkajsdf";
+let division=texto1/texto2;
 let resultado= numero1*numero2;
+document.write(division);
+document.write("<br>");
+document.write(resultado)
+}
