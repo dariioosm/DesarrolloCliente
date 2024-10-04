5657ae101e6e7a4fe183e8624433e5fcc0863171
Dario Sanchez <dariosanchez351@gmail.com>
2024-10-04 12:37:34 +0200

    resoluciones

---
+++ Unidad2/Ejercicios/ejercicio4/ejercicio4.js
@@ -0,0 +1,9 @@
+function cuestionario(){
+   
+    
+    let nombre= prompt ("Introduce tu nombre");
+    let edad=prompt("introduce tu edad");
+    parseInt(edad);
+    let dias=edad*365;
+    document.write(dias);
+}
