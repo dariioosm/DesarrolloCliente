7cd62237c5f4011f01a76a9d74fe880eeb7812e7
Dario Sanchez <dariosanchez351@gmail.com>
2024-10-29 11:08:37 +0100

    comienzo del ejercicio13

---
+++ Unidad3/ejercicio13/alumno.js
@@ -0,0 +1,12 @@
+class Alumno{
+     constructor(nombre,nota1,nota2,nota3){
+        this.nombre=nombre;
+        this.nota1=nota1;
+        this.nota2=nota2;
+        this.nota3=nota3;
+     }
+      function annadir(){
+
+     }
+
+}
