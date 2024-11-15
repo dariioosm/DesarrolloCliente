1ad15ff9a5ebca7994209074d1110fbd6916a0d0
Dario Sanchez <dariosanchez351@gmail.com>
2024-11-15 12:22:30 +0100

    manipulacion del odm

--- Unidad4/pruebasDOM/unindad4.js
+++ Unidad4/pruebasDOM/unindad4.js
@@ -1,11 +1,14 @@
+
+// ! esta funcion discrimina mayus y minus
 const documentoContiene=(nodo,cadena)=>{
     if(nodo.nodeType == Node.ELEMENT_NODE){
         for(let i=0; i<nodo.childNodes.length;i++){
             if(documentoContiene(nodo.childNodes[i],cadena)){
                 return true;
             }
-            return false;
+          
         }
+        return false;
     }else if(nodo.nodeType==Node.TEXT_NODE){
         return nodo.nodeValue.indexOf(cadena)>-1;
     }
