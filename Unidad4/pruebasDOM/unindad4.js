2fd1320cd2a82e9718c2dfe948d7e9f4b026b722
Dario Sanchez <dariosanchez351@gmail.com>
2024-11-15 11:58:00 +0100

    funciones con nodos

---
+++ Unidad4/pruebasDOM/unindad4.js
@@ -0,0 +1,12 @@
+const documentoContiene=(nodo,cadena)=>{
+    if(nodo.nodeType == Node.ELEMENT_NODE){
+        for(let i=0; i<nodo.childNodes.length;i++){
+            if(documentoContiene(nodo.childNodes[i],cadena)){
+                return true;
+            }
+            return false;
+        }
+    }else if(nodo.nodeType==Node.TEXT_NODE){
+        return nodo.nodeValue.indexOf(cadena)>-1;
+    }
+}
